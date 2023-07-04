fff8ac6ecf4c07fcad976f5a1c6657c1d2eac13c
KomorebiHl <821890952@qq.com>
2023-07-04 23:03:57 +0800

    安装ant-design-vue组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia' //插件-提供共享状态的功能
 
 import App from './App.vue' //组件 一个 .vue就是一个组件 根组件-整个应用最底层的组件
 import router from './router' // 路由- SPA-Single Page Application 项目-地址-组件
+import AntD from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
 
 const app = createApp(App) //创建一个vue3的应用实例 对象 实例
 
 app.use(createPinia()) //先创建pinia实例,再完成它的注册
 app.use(router) //注册路由 vue2-vue.use() vue3-app.use()-注册插件
+app.use(AntD)
 
 app.mount('#app') //mount挂载的意思 app实例最终要挂载到哪个dom元素上
--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          'primary-color': '#0960bd' // 配置主题的主色调
+        },
+        javascriptEnabled: true
+      }
     }
   }
 })
