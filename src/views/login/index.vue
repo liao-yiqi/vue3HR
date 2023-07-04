17e053a1dd3a39586f81466105ebb233d8c3b811
KomorebiHl <821890952@qq.com>
2023-07-04 23:31:10 +0800

    feat:登录页基础样式布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,3 +1,62 @@
 <template>
-  <div>登录</div>
+  <div class="login-container">
+    <div class="logo">
+      <div class="icon"></div>
+      <img src="@/assets/common/login.svg" width="300" alt="" />
+      <p>开箱即用的中后台管理系统</p>
+    </div>
+    <div class="form">
+      <h3>iHRM 人力资源管理系统</h3>
+      <a-card class="login-card">
+         登录表单
+      </a-card>
+    </div>
+  </div>
 </template>
+
+<style lang="less">
+.login-container {
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+  .logo {
+    flex: 3;
+    background: rgba(38, 72, 176) url(../../assets/common/logBg.png) no-repeat center / cover;
+    border-top-right-radius: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+    padding: 0 100px;
+    .icon {
+      background: url(../../assets/common/logo.png) no-repeat 70px center / contain;
+      width: 300px;
+      height: 50px;
+      margin-bottom: 50px;
+    }
+    p {
+      color: #fff;
+      font-size: 18px;
+      margin-top: 20px;
+      width: 300px;
+      text-align: center;
+    }
+  }
+  .form {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 50px;
+    .ant-card {
+      width: 320px;
+      border: none;
+      padding: 0;
+    }
+    h3 {
+      padding-left: 30px;
+      font-size: 24px;
+    }
+  }
+}
+</style>
