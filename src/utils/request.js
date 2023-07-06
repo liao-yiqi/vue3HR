9b3b9e20d439e19c5e85602fd7f76662cfb37899
KomorebiHl <821890952@qq.com>
2023-07-06 21:04:40 +0800

    feat:封装request

---
+++ src/utils/request.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+//使用axios创建实例 new Vue() createApp()
+const serive = axios.create({
+  //初始化参数
+})
+//请求拦截器
+serive.interceptors.request.use()
+//响应拦截器
+serive.interceptors.response.use()
+//导出工具
+export default serive
