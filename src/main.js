1a3018310a994cf4e0c8aa2f3885dd2e4ddafd32
HL <821890952@qq.com>
2023-07-11 20:48:40 +0800

    引入antd图标库

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import AntDesign from 'ant-design-vue' // 引入全局包
-import 'ant-design-vue/dist/antd.less' // less - css的预处理器 可以写嵌套语法 可以写变量
-import PluginState from 'pinia-plugin-persistedstate'
-import App from './App.vue'
-import router from './router'
-import '@/permission'
+import AntDesign from "ant-design-vue"; // 引入全局包
+import "ant-design-vue/dist/antd.less"; // less - css的预处理器 可以写嵌套语法 可以写变量
+import PluginState from "pinia-plugin-persistedstate";
+import App from "./App.vue";
+import router from "./router";
+import "@/permission";
+import Icons from "@/components/Icons";
 
-const app = createApp(App)
-const piniaApp = createPinia()
-piniaApp.use(PluginState) //注册持久化插件
-app.use(piniaApp) // 注册pinia
-app.use(router)
-app.use(AntDesign) // 注册全局组件
+const app = createApp(App);
+const piniaApp = createPinia();
+piniaApp.use(PluginState); //注册持久化插件
+app.use(piniaApp); // 注册pinia
+app.use(router);
+app.use(AntDesign); // 注册全局组件
+app.use(Icons); //注册图标
 
-app.mount('#app')
+app.mount("#app");
