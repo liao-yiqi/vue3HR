f79e7d7d9b1c044ccb1b42823a494bee119d8818
HL <821890952@qq.com>
2023-07-16 10:12:55 +0800

    更改配置文件

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -10,23 +10,23 @@
   </div>
 </template>
 <script setup>
-  import { ref } from "vue";
+import { ref } from 'vue'
 
-  const columns = [
-    { title: "序号" },
-    { title: "角色名称", dataIndex: "name" },
-    { title: "角色描述", dataIndex: "description" },
-    { title: "操作", dataIndex: "operate" },
-  ];
-  const list = ref([]);
+const columns = [
+  { title: '序号' },
+  { title: '角色名称', dataIndex: 'name' },
+  { title: '角色描述', dataIndex: 'description' },
+  { title: '操作', dataIndex: 'operate' }
+]
+const list = ref([])
 </script>
 <style lang="less" scoped>
-  .container {
+.container {
+  padding: 16px;
+  .setting-tab {
+    background-color: #fff;
     padding: 16px;
-    .setting-tab {
-      background-color: #fff;
-      padding: 16px;
-      height: calc(100vh - 93px);
-    }
+    height: calc(100vh - 93px);
   }
+}
 </style>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
       reactivityTransform: true,
     }),
     vueJsx(),
+    eslintPlugin(),
   ],
   resolve: {
     alias: {
